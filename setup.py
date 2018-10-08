e42dd2957627be23543a8ddc924a30b7172b9d4e
Abin Shahab <ashahab@linkedin.com>
2018-10-08 14:17:07 -0700

    fixup! fixup! fixup! fixup! fixup! Adding a configMap builder

--- setup.py
+++ setup.py
@@ -12,16 +12,12 @@ setuptools.setup(
     include_package_data=False,
     zip_safe=False,
     classifiers=(
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 2",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ),
     install_requires=[
-        'docker==3.4.1',
         'redis==2.10.6',
-        'notebook==5.6.0',
-        'jupyter==1.0.0',
-        'numpy==1.15.0',
         'kubernetes==6.0.0'
     ],
     extras_require={
