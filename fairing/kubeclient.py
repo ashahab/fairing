ea605fdee93bf878ceb253cb6215f5099a9647b1
Abin Shahab <ashahab@linkedin.com>
2018-11-06 22:52:20 -0800

    fixup! fixup! Merge branch 'master' of github.com:ashahab/fairing-1 into abin-k8s-client

--- fairing/kubeclient.py
+++ fairing/kubeclient.py
@@ -65,14 +65,13 @@ class KubeClient(object):
     def logs(self, name, namespace):
         tail = None
         v1 = kube_client.CoreV1Api()
-        ev = kube_client.EventsV1beta1Api()
         # Retry to allow starting of pod
         # TODO Use urllib3's retry
         retries = MAX_RETRIES
         while retries > 0:
             try:
                 w = watch.Watch()
-                for event in w.stream(ev.read_namespaced_event, namespace=namespace, field_selector="involvedObject.name={}".format(name)):
+                for event in w.stream(v1.read_namespaced_event, namespace=namespace, field_selector="involvedObject.name={}".format(name)):
                     logger.info("Event: %s %s", event['type'], event['reason'])
                     if event['type'] == 'Normal' and event['reason'] == 'Started':
                         tail = v1.read_namespaced_pod_log(name, namespace, follow=True, _preload_content=False)
